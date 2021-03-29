3928db001afde9fe4473fad24e273326d173c243
Daniel A <dalamo90@gmail.com>
2021-03-29 15:39:14 -0500

    modified game and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,85 @@
-//Captains log: I pseudocoded my reset function and button because i wanted to complete everything else before
-//I could not get each gem to add on to the total with each click. 
-//I attempted to create winning/lossing conditions but cannot test them without the players total to actually work
+let wins = 0;
+let losses = 0;
+let compChoice;
+let btnOne;
+let btnTwo;
+let btnThree;
+let btnFour;
+let playerTotal = 0;
+const playersInputSpan = document.getElementById("playersInput");
+const winsSpan = $("#wins");
+const lossesSPan = $("#losses");
+const playerTotalSpan = $("#playersInput");
+const compNum = $("#compsChoice");
+const b1 = document.getElementById("btn-1");
+const b2 = document.getElementById("btn-2");
+const b3 = document.getElementById("btn-3");
+const b4 = document.getElementById("btn-4");
 
+//Comp generates random number
+function randNum (min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+} 
 
-//variables
-var wins = 0;
-var losses = 0;
-var playersInputSpan = document.getElementById("playersInput");
-var winsSpan = $("#wins");
-var lossesSPan = $("#losses");
-var totalSpan = document.getElementById("playerTotal");
+compChoice = randNum(50,100);
+compNum.text(compChoice);
 
+//Generate random numbers for buttons
+btnOne = randNum(1, 12);
+btnTwo = randNum(1, 12);
+btnThree = randNum(1, 12);
+btnFour = randNum(1, 12);
+console.log("This is btn1 " + btnOne);
+console.log("This is btn2 " + btnTwo);
+console.log("This is btn3 " + btnThree);
+console.log("This is btn4 " + btnFour);
 
-//numbers to choose from
-var computerChoices = [42, 56, 69, 119, 111, 20, 21, 99, 77];
-
-//run reset function
-// reset()
-
-
-//each button is a random number each round
-var randomNumber = Math.floor(Math.random() * computerChoices.length);
-console.log(randomNumber);
-var computerGuess =computerChoices[randomNumber];
-console.log(computerGuess);
-$("#playersInput").text(computerGuess);
-
-//player pushes button to generate random #1-12
-//then link each button with diff random number
-//display number each button is given
-var buttonOne = Math.floor(Math.random() * 12) + 1;
-var buttonTwo = Math.floor(Math.random() * 12) + 1;
-var buttonThree = Math.floor(Math.random() * 12) + 1;
-var buttonFour = Math.floor(Math.random() * 12) + 1;
+//assigning values to btns
+b1.value = btnOne;
+b2.value = btnTwo;
+b3.value = btnThree;
+b4.value = btnFour;
 
 $(document).ready(function () {
-var playerTotal = 0;
-var playerTotalSpan = $("#playerTotal");
+$("button").on("click", function(){
+    //convert string value to int
+    playerTotal += parseInt(this.value); 
+    playerTotalSpan.text(playerTotal);
+    score();
+});
 
-$("#btn-1").on("click", function (){
-    playerTotal = buttonOne;
-    playerTotalSpan.text(playerTotal);
-});
-$("#btn-2").on("click", function (){
-    playerTotal = buttonTwo;
-    playerTotalSpan.text(playerTotal);
-});
-$("#btn-3").on("click", function (){
-    playerTotal = buttonThree;
-    playerTotalSpan.text(playerTotal);
-});
-$("#btn-4").on("click", function (){
-    playerTotal = buttonFour;
-    playerTotalSpan.text(playerTotal);
-    console.log("playerTotalSpan " + playerTotalSpan);
-});
-//pseudocoded my reset button until everything else is done
-//$("#reset").on("click", function () {
-//    playerTotal = 0;
-//    playerTotalSpan.text(0);
-//});
-});
-//how to get each button to add up?
-//make all buttons add to players total score
-var sumTotal = buttonOne + buttonTwo + buttonThree + buttonFour;
-console.log("This is sumTotal " + sumTotal);
+function score(){
+//win and lose logic
+if (playerTotal > compChoice) {
+    losses++;
+    lossesSPan.text(losses);  
+    alert("Oh no! You're crystals spilled all over the place.");
 
-//player number has to equal computers
-//wins or losses
-if (playerTotal === computerGuess) {
+    compChoice = randNum(50,100);
+    compNum.text(compChoice);
+    
+    return playerTotal = 0;
+
+} else if (playerTotal === compChoice){
     //player wins
     wins++;
-    $("#results").text("YOU WIN!");
-    //player loses
-} else {
-    losses++;
-    $("#results").text("YOU LOSE!");
-    break;
-};
-    
+    winsSpan.text(wins);
+    alert("Congrats, You didn't spill any crystals!");
 
-//game restarts when player wins/losses
+    compChoice = randNum(50,100);
+    compNum.text(compChoice);
+    
+    return playerTotal = 0;
+} else{
+    return;
+};
+}
+
+//resets game
+$("#reset").on("click", function () {
+   playerTotal = 0;
+   playerTotalSpan.text(0);
+   location.reload();
+});
+
+});
